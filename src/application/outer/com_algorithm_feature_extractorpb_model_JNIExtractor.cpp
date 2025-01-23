8aa44c327d4295a0b86cb1ed3b8f21cc823ae2ea
YouChouNoBB <384375530>
2025-01-23 22:46:47 +0800

    update

--- src/application/outer/com_algorithm_feature_extractorpb_model_JNIExtractor.cpp
+++ src/application/outer/com_algorithm_feature_extractorpb_model_JNIExtractor.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Bryan Zhou on 2022/10/31.
 //
-#include "com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor.h"
+#include "com_algorithm_feature_extractorpb_model_JNIExtractor.h"
 #include "outer.h"
 
 /**
@@ -11,7 +11,7 @@
  * @param class_name 类名
  * @return 指针
  */
-JNIEXPORT jlong JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor_genInstance
+JNIEXPORT jlong JNICALL Java_com_algorithm_feature_extractorpb_model_JNIExtractor_genInstance
         (JNIEnv * env, jobject obj, jstring class_name){
     const char* name = env->GetStringUTFChars(class_name,0);
     Extractor *e = instanceMap[string(name)];
@@ -27,7 +27,7 @@ JNIEXPORT jlong JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_J
  * @param size 字节数组长度
  * @return 抽取结果，字节数组
  */
-JNIEXPORT jbyteArray JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor_extract
+JNIEXPORT jbyteArray JNICALL Java_com_algorithm_feature_extractorpb_model_JNIExtractor_extract
         (JNIEnv * env, jobject obj, jlong instance, jbyteArray data, jint size){
     if(env->GetArrayLength(data)!=size){
         cout<<"size not equal";
@@ -60,7 +60,7 @@ JNIEXPORT jbyteArray JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_mo
  * @param size 字节数组长度
  * @return sample
  */
-JNIEXPORT jbyteArray JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor_extractBatch
+JNIEXPORT jbyteArray JNICALL Java_com_algorithm_feature_extractorpb_model_JNIExtractor_extractBatch
         (JNIEnv * env, jobject obj, jlong instance, jbyteArray data, jint size){
     if(env->GetArrayLength(data)!=size){
         cout<<"size not equal";
@@ -90,7 +90,7 @@ JNIEXPORT jbyteArray JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_mo
  * @param obj
  * @param instance 对象指针
  */
-JNIEXPORT void JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor_delete
+JNIEXPORT void JNICALL Java_com_algorithm_feature_extractorpb_model_JNIExtractor_delete
         (JNIEnv * env, jobject obj, jlong instance){
     Extractor *e = reinterpret_cast<Extractor*>(instance);
     if(e)
@@ -104,7 +104,7 @@ JNIEXPORT void JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JN
  * @param obj
  * @param data sample
  */
-JNIEXPORT void JNICALL Java_com_shoplazza_algorithm_feature_extractorpb_model_JNIExtractor_release
+JNIEXPORT void JNICALL Java_com_algorithm_feature_extractorpb_model_JNIExtractor_release
         (JNIEnv * env, jobject obj, jbyteArray data){
     jbyte * olddata = (jbyte*)env->GetByteArrayElements(data, 0);
     env->ReleaseByteArrayElements(data,olddata,0);
